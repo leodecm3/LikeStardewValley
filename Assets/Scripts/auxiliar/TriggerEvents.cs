da56b15d662fab8cd095bc39d54cb6dcd1f1751d
Leo - Leonardo Mezavilla <30425975+leodecm3@users.noreply.github.com>
2023-01-07 12:00:07 -0500

    fix the trigger events

--- Assets/Scripts/auxiliar/TriggerEvents.cs
+++ Assets/Scripts/auxiliar/TriggerEvents.cs
@@ -13,21 +13,19 @@ public enum TriggerEnum {
 public class TriggerEvents : MonoBehaviour {
 
     
-    public UnityEvent<Collider2D,TriggerEnum> onTriggerEnter2DEvent;
-    public UnityEvent<Collider2D,TriggerEnum> onTriggerExit2DEvent;
-    public UnityEvent<Collider2D,TriggerEnum> onTriggerStay2DEvent;
+    public UnityEvent<Collider2D,TriggerEnum> onTriggerEvent;
     
     
     private void OnTriggerEnter2D(Collider2D col) {
-        onTriggerEnter2DEvent.Invoke(col,TriggerEnum.Enter);
+        onTriggerEvent.Invoke(col,TriggerEnum.Enter);
     }
     
     private void OnTriggerExit2D(Collider2D col) {
-        onTriggerExit2DEvent.Invoke(col,TriggerEnum.Exit);
+        onTriggerEvent.Invoke(col,TriggerEnum.Exit);
     }
 
     private void OnTriggerStay2D(Collider2D col) {
-        onTriggerStay2DEvent.Invoke(col,TriggerEnum.Stay);
+        onTriggerEvent.Invoke(col,TriggerEnum.Stay);
     }
     
 
