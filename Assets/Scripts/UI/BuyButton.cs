9de1955ac22368d21b20303bb19e923fb63d912c
Leo - Leonardo Mezavilla <30425975+leodecm3@users.noreply.github.com>
2023-01-07 12:02:22 -0500

    in screen button to buy

--- Assets/Scripts/UI/BuyButton.cs
+++ Assets/Scripts/UI/BuyButton.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class BuyButton : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class BuyButton : MonoBehaviour {
+    [SerializeField] private GameObject xNotEnoughMoney;
+    [SerializeField] private TextMeshProUGUI price;
+    [SerializeField] private Image image;
+    [SerializeField] private float totalPrice;
+    [SerializeField] private int qtd;
+    [SerializeField] private SOobject so;
+    
+    private GameManager _gm;
+    
+    
+    private void Start() {
+        _gm = GameManager.Instance;
+        UpdateUI();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnEnable() {
+        UpdateUI();
     }
+
+    private void UpdateUI() {
+        price.SetText("$"+ totalPrice + " = " + qtd + "x");
+        image.sprite = so.sprite;
+        
+        //put the X case there is not enough money
+        xNotEnoughMoney.SetActive(_gm.GetPlayersMoney() < totalPrice);
+
+    }
+
+
+    public void OnBuyThis() {
+
+        
+
+
+        UpdateUI();
+    }
+    
+
+    
+    
+    
+
+
+
+
+
+
+
+
 }
